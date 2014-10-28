84889eb8390d7afca99d74ec863163a1d24cce76
Jackson-Ou <635712390@qq.com>
2014-10-28 16:43:55 +0800

    fourth commit

--- Study_Selenium/testcase/my_test1.py
+++ Study_Selenium/testcase/my_test1.py
@@ -9,8 +9,7 @@ import unittest, time, re
 import HTMLTestRunner
 
 class my_test1(unittest.TestCase):
-    u"""我的测试11"""
-    u"""我的测试11"""
+    u"""我的测试1"""
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(15)
